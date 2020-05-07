655f56b300d0f27026e2f5d51151a2d88933cd88
Rene Avila <reneavila1993@gmail.com>
2020-05-07 18:48:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -12,9 +12,9 @@ def find_element_index(array, value_to_find)
   index = 0 
   while index < array.length do 
     if array[index] == value_to_find
-      return index 
-      index += 1
+      return index
     end
+    index += 1
   end
 end
 
